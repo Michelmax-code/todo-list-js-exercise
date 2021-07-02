bfb498dc8c43e4cb13d10c9cf70195896437796a
Miguel Jimenez <migang9@gmail.com>
2021-07-02 18:26:16 -0400

    todo list OO

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const taskComplete = [];
 const taskDescriptions = [];
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = (title, description) => {
   const task =  {
     title: title,
     description: description,
@@ -18,7 +18,7 @@ function newTask(title, description) {
     }
   };
   return task;
-}
+};
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 // Print the state of a task to the console in a nice readable way
 // DRIVER CODE BELOW
@@ -30,3 +30,4 @@ task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logState(); // Clean Cat Litter has been completed
 
+console.log(tasks);
